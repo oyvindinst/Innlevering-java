93ae22c796b96febb086c37de53f5b2ec096fc81
oyvindinst <70864525+oyvindinst@users.noreply.github.com>
2020-11-12 23:51:27 +0100

    Add files via upload

---
+++ dat100-jplab11-startkode/src/no/hvl/dat100/jplab11/oppgave1/Innlegg.java
@@ -0,0 +1,88 @@
+package no.hvl.dat100.jplab11.oppgave1;
+
+import no.hvl.dat100.jplab11.common.TODO;
+
+public abstract class Innlegg {
+	
+	// TODO - deklarering av objektvariable
+	
+	private int id;
+	private String bruker;
+	private String dato;
+	private int likes;
+	
+	public Innlegg() {
+		
+	}
+	
+	public Innlegg(int id, String bruker, String dato) {
+
+		// TODO 
+		this.id=id;
+		this.bruker=bruker;
+		this.dato=dato;
+		likes=0;
+	}
+
+	public Innlegg(int id, String bruker, String dato, int likes) {
+
+		// TODO - START
+		
+		this.id=id;
+		this.bruker=bruker;
+		this.dato=dato;
+		this.likes=likes;
+	}
+	
+	public String getBruker() {
+		
+		return bruker;
+
+	}
+
+	public void setBruker(String bruker) {
+		this.bruker=bruker;
+	}
+
+	public String getDato() {
+		return dato;
+		
+	}
+
+	public void setDato(String dato) {
+		this.dato = dato;
+	}
+
+	public int getId() {
+		return id;
+
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+	
+	public void doLike () {
+		throw new UnsupportedOperationException(TODO.method());
+	}
+	
+	public boolean erLik(Innlegg innlegg) {
+		throw new UnsupportedOperationException(TODO.method());
+
+	}
+	
+	@Override
+	public String toString() {
+		
+		String str = id + "\n" + bruker + "\n" + dato + "\n" + likes + "\n";
+				
+		return str;
+	}
+	
+	// Metoden nedenfor er kun for valgfri oppgave 6
+	public String toHTML() {
+		
+		throw new UnsupportedOperationException(TODO.method());
+				
+	}
+}
