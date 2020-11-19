6affdcfbecb1fd9dbefcc372434f8932a6d091f0
oyvindinst <70864525+oyvindinst@users.noreply.github.com>
2020-11-19 23:32:17 +0100

    Add files via upload

---
+++ dat100-jplab11-startkode/jplab11/oppgave1/Innlegg.java
@@ -0,0 +1,92 @@
+package no.hvl.dat100.jplab11.oppgave1;
+
+import no.hvl.dat100.jplab11.common.TODO;
+
+public abstract class Innlegg {
+	
+	// TODO - deklarering av objektvariable
+	
+	private int id;
+	private String bruker;
+	private String dato;
+	private int likes;
+	
+	public Innlegg() {
+		
+	}
+	
+	public Innlegg(int id, String bruker, String dato) {
+
+		// TODO 
+		this.id=id;
+		this.bruker=bruker;
+		this.dato=dato;
+		this.likes=0;
+	}
+
+	public Innlegg(int id, String bruker, String dato, int likes) {
+
+		// TODO - START
+		
+		this.id=id;
+		this.bruker=bruker;
+		this.dato=dato;
+		this.likes=likes;
+	}
+	
+	public String getBruker() {
+		
+		return bruker;
+
+	}
+
+	public void setBruker(String bruker) {
+		this.bruker=bruker;
+	}
+
+	public String getDato() {
+		return dato;
+		
+	}
+
+	public void setDato(String dato) {
+		this.dato = dato;
+	}
+
+	public int getId() {
+		return id;
+
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+	
+	public void doLike () {
+		
+	likes++;
+	}
+	public boolean erLik(Innlegg innlegg) {
+		
+if (innlegg.getId() == id) {
+	return true;
+}else{return false;
+	
+}
+	}
+	
+	@Override
+	public String toString() {
+		
+		String str = id + "\n" + bruker + "\n" + dato + "\n" + likes + "\n";
+				
+		return str;
+	}
+	
+	// Metoden nedenfor er kun for valgfri oppgave 6
+	public String toHTML() {
+		
+		throw new UnsupportedOperationException(TODO.method());
+				
+	}
+}
---
+++ dat100-jplab11-startkode/jplab11/oppgave3/Blogg.java
@@ -0,0 +1,104 @@
+package no.hvl.dat100.jplab11.oppgave3;
+
+import no.hvl.dat100.jplab11.common.TODO;
+import no.hvl.dat100.jplab11.oppgave1.*;
+
+public class Blogg {
+
+	// TODO: objektvariable 
+	
+private Innlegg[] innleggstabell;
+private int nesteledig;
+
+	public Blogg() {
+		innleggstabell = new Innlegg[20];
+	}
+
+	public Blogg(int lengde) {
+		innleggstabell = new Innlegg[lengde];
+		nesteledig=0;
+	}
+
+	public int getAntall() {
+		return nesteledig;
+	}
+	
+	public Innlegg[] getSamling() {
+		return innleggstabell;
+
+	}
+	
+	public int finnInnlegg(Innlegg innlegg) {
+
+		int indeks = -1;
+		for (int i = 0; i < nesteledig; i++) {
+			if (innleggstabell[i].erLik(innlegg)) {
+				
+				indeks=i;
+			}
+		}
+		return indeks;
+	}
+
+	public boolean finnes(Innlegg innlegg) {
+		for (int i = 0; i < nesteledig; i++) {
+			if (innleggstabell[i] != null && (innleggstabell[i].erLik(innlegg))) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean ledigPlass() {
+		
+		for (int i = 0; i < innleggstabell.length-1; i++) {	
+			if (innleggstabell[i] == null) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+	
+	public boolean leggTil(Innlegg innlegg) {
+
+		if(ledigPlass() == true ) {
+			innleggstabell[nesteledig] = innlegg;
+			nesteledig++;
+			return true;
+			}
+			return false;
+		}
+	
+	
+	public String toString() {
+		String str = nesteledig +"\n"+"";
+		for (int i = 0; i < nesteledig; i++) {
+			str += innleggstabell[i].toString();
+		}
+		return str;
+	}
+
+	// valgfrie oppgaver nedenfor
+	
+	public void utvid() {
+		throw new UnsupportedOperationException(TODO.method());
+	}
+	
+	public boolean leggTilUtvid(Innlegg innlegg) {
+
+		throw new UnsupportedOperationException(TODO.method());
+		
+	}
+	
+	public boolean slett(Innlegg innlegg) {
+		
+		throw new UnsupportedOperationException(TODO.method());
+	}
+	
+	public int[] search(String keyword) {
+		
+		throw new UnsupportedOperationException(TODO.method());
+
+	}
+}
